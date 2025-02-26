3c0356c42b71c8c1c5de464bf54f0d90c2a0b803
monumentum-dev <stronamonumentum@gmail.com>
2025-02-26 08:16:39 +0100

    init

---
+++ src/components/Form.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('/.netlify/functions/hello')
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>React + Netlify Functions</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
