6152494711e1ed9b6b71404d474eb55d2b813911
monumentum-dev <stronamonumentum@gmail.com>
2025-02-26 08:27:46 +0100

    function test 1

--- src/netlify/functions/hello.js
+++ src/netlify/functions/hello.js
@@ -1,6 +1,11 @@
-exports.handler = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Привет от Netlify Function!' }),
-    };
-  };
+
+export default async (request, context) => {
+  try {
+    // const response = await fetch(API_ENDPOINT);
+    // const data = await response.json();
+    return Response.json({ message:"Hello world" });
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: 'Failed fetching data' }, { status: 500 });
+  }
+};
