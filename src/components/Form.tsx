2c1f49eaef17cb19ff9e75fc6ae6c52bb2ac1548
monumentum-dev <stronamonumentum@gmail.com>
2025-02-26 10:02:15 +0100

    test 4

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -3,17 +3,17 @@ import React, { useEffect, useState } from 'react';
 function App() {
   const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    fetch('/.netlify/functions/hello')
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message))
-      .catch((error) => console.error(error));
-  }, []);
+//   useEffect(() => {
+//     fetch('/.netlify/functions/hello')
+//       .then((response) => response.json())
+//       .then((data) => setMessage(data.message))
+//       .catch((error) => console.error(error));
+//   }, []);
    console.log("Message", message)
   return (
     <div>
       <h1>React + Netlify Functions</h1>
-      <p>{message}</p>
+      {/* <p>{message}</p> */}
     </div>
   );
 }
